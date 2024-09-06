80fe4d474eab74dce133c673ebda88afeecda6e7
Dhawal Sarin <dhawal12sarin@gmail.com>
2024-09-06 21:48:19 +0530

    first push x-calculator.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result,setResult] = useState('');
+  const [equation,setEquation] = useState('');
+
+  const handleClick = (e) =>{
+    if(e.target.textContent==='C'){
+      setEquation('');
+      setResult('');
+    }
+    else if(e.target.textContent==='='){
+      if(equation==='')
+        setResult('Error');
+      else
+        setResult(eval(equation));
+    }
+    else
+      setEquation((prevState)=>prevState + e.target.textContent);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <input type='text' readOnly value={equation}/>
+      <div>{result}</div>
+      <div style={{display:'flex', justifyContent:'center'}}>
+        <div style={{width:'350px',height:'350px'}}>
+          <button onClick={handleClick}>7</button>
+          <button onClick={handleClick}>8</button>
+          <button onClick={handleClick}>9</button>
+          <button onClick={handleClick}>+</button>
+          <button onClick={handleClick}>4</button>
+          <button onClick={handleClick}>5</button>
+          <button onClick={handleClick}>6</button>
+          <button onClick={handleClick}>-</button>
+          <button onClick={handleClick}>1</button>
+          <button onClick={handleClick}>2</button>
+          <button onClick={handleClick}>3</button>
+          <button onClick={handleClick}>*</button>
+          <button onClick={handleClick}>C</button>
+          <button onClick={handleClick}>0</button>
+          <button onClick={handleClick}>=</button>
+          <button onClick={handleClick}>/</button>
+        </div>
+      </div>
     </div>
   );
 }
